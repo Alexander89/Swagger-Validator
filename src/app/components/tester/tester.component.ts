83abdb9e51eaa50f261cf7b2347379c418b90684
Alexander Halemba <alexander.halemba@hotelbird.com>
2018-06-13 09:55:47 +0200

    add doku for tester component

--- src/app/components/tester/tester.component.ts
+++ src/app/components/tester/tester.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
@@ -10,14 +10,18 @@ import * as S from '@swagger/swagger';
   templateUrl: './tester.component.html',
   styleUrls: ['./tester.component.css']
 })
-export class TesterComponent implements OnInit, OnChanges {
-
+export class TesterComponent implements OnChanges {
+	/** url to load the swagger definition from */
 	@Input() source: string;
+	/** resource who is loaded actually */
 	private loadSource: string;
+	/** http status for get call */
 	public status: string;
 
+	/** info block from swagger call */
 	protected info: S.Info;
 
+	/** all open subscriptions to clean up on change the view */
 	private openSubscription: Subscription;
 
 	constructor(private cd: ChangeDetectorRef, public swagger: Swagger, protected httpClient: HttpClient) {
@@ -25,10 +29,10 @@ export class TesterComponent implements OnInit, OnChanges {
 		this.status = 'ready';
 	}
 
-	ngOnInit() {
-	}
-
-	ngOnChanges() {
+	/**
+	 * handler when Input is changed or other fields ar changed
+	 */
+	public ngOnChanges() {
 		// on every change check if the source is changed
 		if (this.loadSource !== this.source) {
 			// hand over source to avoid recheck
@@ -53,6 +57,10 @@ export class TesterComponent implements OnInit, OnChanges {
 		}
 	}
 
+	/**
+	 * Load the Json and return an observable to get the Swagger definition
+	 * @param src url to load the JSON file
+	 */
 	private loadJson(src: string): Observable<S.Root> {
 		const headers = new HttpHeaders();
 		headers.append('Access-Control-Allow-Origin', '*');
@@ -66,13 +74,31 @@ export class TesterComponent implements OnInit, OnChanges {
 		);
 	}
 
+	/** getter for the swagger path array */
 	get pathArray() { return this.swagger.pathArray; }
 
+	/**
+	 * Tag tracker for the ngFor loop
+	 * @param index index of the item
+	 * @param tag tag to be tracked
+	 */
 	public trackTag(index, tag: S.Tag) { return tag.name; }
+	/**
+	 * Call tracker for the ngFor loop
+	 * @param index index for the item
+	 * @param call call to be tracked
+	 */
 	public trackCall(index, call) { return call.callName; }
+	/**
+	 * Methode tracker for the ngFor loop
+	 * @param index index of the item
+	 * @param method Method to be tracked
+	 */
 	public trackMeth(index, method) { return method.name; }
 
+	/** getter for the selected scheme */
 	get selectedScheme(): string {return this.swagger.selectedRequestScheme; }
+	/** setter for the selecte scheme */
 	set selectedScheme(value: string) { this.swagger.selectedRequestScheme = value; }
 
 }
